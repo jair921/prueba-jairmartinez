536a69d89af8b6ccbd50eb4c41827f691292ed7e
Jair Andres Martinez Manzano <jamartinez@nexura.com>
2020-08-27 15:50:45 -0500

    feat: Dispaly productos on home page

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -1,4 +1,25 @@
 @extends('layout.master')
 
 @section('content')
+<div class="row featured__filter">
+    @foreach($products as $product)
+        <div class="col-lg-3 col-md-4 col-sm-6 mix vegetables fastfood">
+            <div class="featured__item">
+                <div class="featured__item__pic set-bg" data-setbg="{{asset($product->image)}}">
+                    <ul class="featured__item__pic__hover">
+                        <li>
+                            <a href="#" title="">
+                                <i class="fa fa-shopping-cart" title=""></i>
+                            </a>
+                        </li>
+                    </ul>
+                </div>
+                <div class="featured__item__text">
+                    <h6><a href="#">{{$product->name}}</a></h6>
+                    <h5>$ {{$product->price}}</h5>
+                </div>
+            </div>
+        </div>
+    @endforeach
+</div>
 @endsection
--- routes/web.php
+++ routes/web.php
@@ -13,6 +13,4 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::get('/', function () {
-    return view('welcome');
-})->name('index');
+Route::get('/', 'HomeController@index')->name('index');
