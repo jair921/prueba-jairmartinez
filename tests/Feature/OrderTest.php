5f326d333f1196184db5d86a53b5f5ef47d857a1
Jair Andres Martinez Manzano <martinez.jair8@gmail.com>
2020-08-27 00:30:08 -0500

    test: Add annotation on test function for orders

--- tests/Feature/OrderTest.php
+++ tests/Feature/OrderTest.php
@@ -9,6 +9,7 @@ use Tests\TestCase;
 class OrderTest extends TestCase
 {
 
+     /** @test */
     public function it_visit_page_of_order()
     {
         $response = $this->get('/order?product=1');
