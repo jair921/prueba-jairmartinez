a7f94b3baf610c1d9c7e5999efadc29ffff1505e
Jair Andres Martinez Manzano <martinez.jair8@gmail.com>
2020-08-28 02:48:42 -0500

    fix: remove data gateway

--- config/payments.php
+++ config/payments.php
@@ -1,12 +1,12 @@
 <?php
 
 return [
-    
+
     'methods' => ['placetopay'],
-    
+
     'placetopay' => [
-        'url'=>'https://dev.placetopay.com/redirection/',
-        'login'=>'6dd490faf9cb87a9862245da41170ff2',
-        'trankey'=>'024h1IlD',
+        'url'=>'',
+        'login'=>'',
+        'trankey'=>'',
     ]
 ];
