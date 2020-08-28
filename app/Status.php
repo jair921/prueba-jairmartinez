011969da673aba4515259d0428ab7d589328d808
Jair Andres Martinez Manzano <martinez.jair8@gmail.com>
2020-08-28 00:20:57 -0500

    fix: Add all status

--- app/Status.php
+++ app/Status.php
@@ -8,12 +8,16 @@ namespace App;
  * @author adminnexura
  */
 class Status {
+    public static $CREATED = 'CREATED';
     public static $APPROVED = 'APPROVED';
+    public static $PAYED = 'PAYED';
     public static $REJECTED = 'REJECTED';
     public static $PENDING = 'PENDING';
     
     public static $status = [
+        'CREATED' => 'Creado',
         'APPROVED' => 'Aprobado',
+        'PAYED' => 'Pagado',
         'REJECTED' => 'Rechazado',
         'PENDING' => 'Pendiente',
     ];
