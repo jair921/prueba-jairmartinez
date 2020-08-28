95c0b38d0555d6626d8f29dfdc2ebe91f3e12f91
Jair Andres Martinez Manzano <martinez.jair8@gmail.com>
2020-08-28 00:30:10 -0500

    test: Add test to orders

--- tests/Feature/OrderTest.php
+++ tests/Feature/OrderTest.php
@@ -5,6 +5,7 @@ namespace Tests\Feature;
 use Illuminate\Foundation\Testing\RefreshDatabase;
 use Illuminate\Foundation\Testing\WithFaker;
 use Tests\TestCase;
+use DB;
 
 class OrderTest extends TestCase
 {
@@ -16,4 +17,50 @@ class OrderTest extends TestCase
 
         $response->assertStatus(200);
     }
+
+
+    /** @test */
+    public function it_display_404_page_of_order()
+    {
+        $response = $this->get('/order?product=0');
+
+        $response->assertStatus(404);
+    }
+
+    /** @test */
+    function it_creates_a_new_order()
+    {
+        $this->post('/order', [
+            'customer_name' => 'Jair',
+            'customer_mobile' => '3004038519',
+            'customer_email' => 'martinez.jair8@gmail.com',
+            'method' => 'placetopay',
+            'product' => 1,
+        ])->assertRedirect();
+
+        return DB::table('orders')->orderBy('id', 'desc')->first()->id;
+    }
+
+
+     /**
+      @test
+      @depends it_creates_a_new_order */
+    public function it_load_show_order($id)
+    {
+        $response = $this->get('/order/'.$id);
+
+        $response->assertStatus(200);
+
+        DB::table('orders')->where('id', $id)->delete();
+    }
+
+
+    /** @test */
+    public function it_visit_list_orders()
+    {
+        $response = $this->get('/orders');
+
+        $response->assertStatus(200);
+    }
+
 }
